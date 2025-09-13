ed0abca11229d18469fde65675d3937198f47858
Anant Sajnani <asajnani@gmail.com>
2025-09-13 16:15:24 -0700

    Working version

--- otel-spring-boot-demo/src/main/java/com/example/demo/DemoApplication.java
+++ otel-spring-boot-demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,20 +3,47 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
+import io.micrometer.core.instrument.binder.system.UptimeMetrics;
+import org.springframework.context.annotation.Bean;
+import io.micrometer.core.instrument.binder.MeterBinder;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
+
+
+
     public static void main(String[] args) {
+
         SpringApplication.run(DemoApplication.class, args);
+
     }
+
+  @Bean
+  public List<MeterBinder> systemMetrics() {
+      return List.of(
+          new JvmMemoryMetrics(),
+          new JvmThreadMetrics(),
+          new ProcessorMetrics(),
+          new UptimeMetrics()
+      );
+  }
 }
 
 @RestController
 @RequestMapping("/api")
 class HelloController {
+   private static final Logger logger = LogManager.getLogger(HelloController.class);
 
     @GetMapping("/hello")
     public String hello(@RequestParam(defaultValue = "world") String name) {
+        logger.info("This is a test log " + name);
         return "Hello, " + name + "!";
     }
 
