898a76647880f7cc7a8956f5612d9ca8051ce089
Anant Sajnani <asajnani@gmail.com>
2025-09-14 13:11:35 -0700

    Observability

--- otel-spring-boot-demo/src/main/java/com/example/demo/DemoApplication.java
+++ otel-spring-boot-demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,16 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
-import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
-import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
-import io.micrometer.core.instrument.binder.system.UptimeMetrics;
 import org.springframework.context.annotation.Bean;
-import io.micrometer.core.instrument.binder.MeterBinder;
-import java.util.List;
-import java.net.InetAddress;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+
 
 @SpringBootApplication
 public class DemoApplication {
@@ -23,29 +15,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-    @Bean
-     public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
-         String hostname;
-         try {
-             hostname = InetAddress.getLocalHost().getHostName();
-         } catch (Exception e) {
-             hostname = "unknown";
-         }
-         final String finalHostname = hostname;
 
-
-         return registry -> registry.config().commonTags("hostname", finalHostname);
-     }
-
-  @Bean
-  public List<MeterBinder> systemMetrics() {
-      return List.of(
-          new JvmMemoryMetrics(),
-          new JvmThreadMetrics(),
-          new ProcessorMetrics(),
-          new UptimeMetrics()
-      );
-  }
 }
 
 @RestController
